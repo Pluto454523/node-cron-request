d7b60208be28821c737b7af1abfcc0170dfd6990
PLUTo <nawin3399@gmail.com>
2023-02-09 12:20:40 +0700

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request = require('request');
 const config = require('./config');
 const { JOB_SCHEDULE, USERNAME_KMUTNB, PASSWORD_KMUTNB } = config;
 
-const app = require('./index');
+const app = require('./api');
 const { getSession } = app;
 
 
