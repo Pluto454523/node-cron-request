862d9208c62a89654f30161659c5e8c8756f3f8e
PLUTo <nawin3399@gmail.com>
2023-02-28 22:10:52 +0700

    refactor code

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const cron = require('node-cron');
-const request = require('request');
+const cron      = require('node-cron');
+const config    = require('./app_config');
+const app       = require('./api');
 
-const config = require('./app_config');
 const { JOB_SCHEDULE, USERNAME_KMUTNB, PASSWORD_KMUTNB } = config;
-
-const app = require('./api');
 const { getSession } = app;
 
 cron.schedule(JOB_SCHEDULE, () => {
 
-    let fs = require('fs');
     const d = new Date();
     let h = d.getHours() < 10 ? "0" + d.getHours() : d.getHours();
     let m = d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes();
     let s = d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds();
     let time = h + ":" + m + ":" + s;
 
+    let fs = require('fs');
     fs.appendFile('log.txt', `[${time}] Runing task\n`, function (err) {
         if (err) throw err;
         console.log(`[${time}] Runing task`)
@@ -25,17 +23,3 @@ cron.schedule(JOB_SCHEDULE, () => {
     scheduled: true,
     timezone: "Asia/bangkok"
 });
-
-// cron.schedule('*/1 * * * * *', () => {
-
-//     const d = new Date();
-//     let h = d.getHours();
-//     let m = d.getMinutes();
-//     let s = d.getSeconds();
-//     let time = h + ":" + m + ":" + s;
-
-//     console.log(time);
-// }, {
-//     scheduled: true,
-//     timezone: "Asia/bangkok"
-// });
